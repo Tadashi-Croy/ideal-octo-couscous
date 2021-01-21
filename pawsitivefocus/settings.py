e7ac2d803ca76666b29b4a3accaf2d55865e1de4
Tadashi-Croy <Tadashicroy@gmail.com>
2021-01-20 18:57:47 -0800

    dep changes

--- pawsitivefocus/settings.py
+++ pawsitivefocus/settings.py
@@ -45,7 +45,7 @@ SECRET_KEY= config('DJANGO_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '104.225.208.26','https://pawsitive-focus.herokuapp.com/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
